7fed74491fb58b097890f04bcce8b22fa2569d16
anthomaxcool <anthonysylvain2009@hotmail.fr>
2015-03-13 13:51:36 -0400

    Skelette Shape.cpp

--- Shape/shape.cpp
+++ Shape/shape.cpp
@@ -10,3 +10,35 @@ Shape::~Shape()
 {
 
 }
+void Shape::draw()
+{
+
+}
+void Shape::setLineColor()
+{
+
+}
+Color Shape::getLineColor()
+{
+
+}
+void Shape::setFillColor()
+{
+
+}
+Color Shape::getFillColor()
+{
+
+}
+Point Shape::getPoint(unsigned int _index)
+{
+
+}
+unsigned int Shape::getNumberOfPoints()
+{
+
+}
+void Shape::add()
+{
+
+}
